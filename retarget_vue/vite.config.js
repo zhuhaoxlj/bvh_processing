e253d331fdc52af24c116212931a987b962a43f4
MarkGosling <1710589105@qq.com>
2026-08-24 21:05:21 +0800

    chore(retarget): remove Vue motion preview

--- retarget_vue/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-export default defineConfig({
-  plugins: [vue()],
-  publicDir: "public",
-  server: { host: "127.0.0.1", port: 5174 },
-});
